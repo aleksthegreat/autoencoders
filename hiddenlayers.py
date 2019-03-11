47e7dd81864b002927cc9543bb0e7caefaccf691
aleksthegreat <aleksjovanovich@gmail.com>
2019-03-10 23:04:04 -0400

    Create hiddenlayers.py

---
+++ hiddenlayers.py
@@ -0,0 +1,16 @@
+Python
+def FEATURES(model):
+    input_ = model.get_layer('inputs').input
+    feat1 = model.get_layer('dense1').output
+    feat2 = model.get_layer('dense2').output
+    feat3 = model.get_layer('dense3').output
+    feat = Concatenate(name='concat')([feat1, feat2, feat3])
+    model = Model(inputs=[input_],
+                      outputs=[feat])
+    return model
+
+_model = FEATURES(autoenc)
+features_train = _model.predict(x_train)
+features_test = _model.predict(x_test)
+print(features_train.shape, ' train samples shape')
+print(features_test.shape, ' train samples shape')
